78ee55b41666bd6952057cda2ad6bd3a173c9425
Irving Dian Pratama <irving.dianpratama@gmail.com>
2019-10-31 23:26:54 +0700

    first commit

---
+++ app/src/main/java/com/irving/funfacts/MainActivity.java
@@ -0,0 +1,44 @@
+ package com.irving.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+ public class MainActivity extends AppCompatActivity {
+     private FactBook factBook = new FactBook();
+     private ColorWheel colorWheel = new ColorWheel();
+    // declare our view variable
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        factTextView = (TextView) findViewById(R.id.factTextView);
+        showFactButton = (Button) findViewById(R.id.showFactButton);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = factBook.getFact();
+                factTextView.setText(fact);
+
+                int color = colorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+
+        showFactButton.setOnClickListener(listener);
+    }
+}
